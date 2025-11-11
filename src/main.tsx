80f51a2dd6be75d647843c682270f934e84e4810
skanehira <sho19921005@gmail.com>
2025-11-11 17:14:47 +0900

    chore: update ci/cd (#3)
    * chore: update ci/cd
    
    * chore: add workflow dispatch to ci
    
    * chore: add fail_level
    
    * fix: ci check
    
    * chore: add pinact config
    
    * chore: add pinact-check

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
-const root = document.getElementById("root")
+const root = document.getElementById("root");
 
 if (!root) throw new Error("Failed to find the root element");
 
 createRoot(root).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</StrictMode>,
 );

8f00eb87c6d535113a4c46f04c6b86cee7a8c326
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-11-11 10:07:07 +0000

    feat: oxc

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/

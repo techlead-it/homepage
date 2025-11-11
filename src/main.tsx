65a0d853d1555b263c6dd0931088f6dacf3d31a8
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-11-11 09:56:45 +0000

    fix: use hash route (#5)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -10,8 +10,8 @@ if (!root) throw new Error("Failed to find the root element");
 
 createRoot(root).render(
 	<StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</StrictMode>,
 );
